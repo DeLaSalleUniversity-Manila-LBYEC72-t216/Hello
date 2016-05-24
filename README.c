efe06abe3c93ca3db123da652a2717a4a11688c7
mnsarias <pem_arias2@yahoo.com>
2016-05-24 16:35:29 +0800

    Create README.c

---
+++ README.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n");
+	printf("\t\t\tMiranda Silva\n");
+	printf("\t\t\tBS CHE\n");
+	printf("\t\t\t11914497\n");
+	printf("\t\t\tMay 24, 2016\n");
+	printf("\t\t\tmns.arias@mail.com\n\n");
+	printf("\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+	return 0;
+}
+...
+
+![](readme.png)
